ed2395cffac8805be7042ebca8dfe6e83b318cb4
Justin Hanley <joevegan@gmail.com>
2016-10-30 14:55:59 -0600

    working script/db

--- linux_deamon/purge.sh
+++ linux_deamon/purge.sh
@@ -3,11 +3,20 @@
 log="/var/log/PurgePlus"
 echo $(date)" Starting PrugePlus" >> $log
 while true
-        do
-        echo $(date) >> $log
-        q=`mysql -s -u purgeu -ppurgepass purgePlus -e "select * from ..."`
-        # finds all modify to use query result in find, and exception
-        # find . ! -name 'file.txt' -type f -exec rm -f {} +
-        echo $q >> $log
-        sleep 5
+  do
+  # for each purge/exception
+  mysql -s -u purgeu -ppurgepass purgePlus -e "select p.dir as dir, \
+                                              group_concat(distinct '-name ', pf.fileName separator ' -o ') as files, \
+                                              group_concat(distinct pe.fileName separator '\|') as exceptions, \
+                                              retentionDays as days \
+                                                from purges p \
+                                                left join purgeFiles pf on pf.purgeId = p.id \
+                                                left join purgeExceptions pe on pe.purgeId = p.id \
+                                                  where systemId = 1 \
+                                                  and enabled = 1 \
+                                                    group by p.id" | while IFS="$( echo -e '\t' )" read dir files exceptions days; do
+  echo "[PurgePlus][exec][$(date)] find $dir -type f $files -mtime +$days | egrep -v $exceptions | xargs rm -f" >> $log
+  find $dir -type f $files -mtime +$days | egrep -v $exceptions | xargs rm -f
+  done
+  sleep 200
 done
--- sql/linuxpurges.sql
+++ sql/linuxpurges.sql
@@ -1,12 +1,14 @@
 /* pruges */
 select
-p.id as purgeId,
-p.dir as path,
-pf.fileName as purgefile
+p.dir,
+group_concat(distinct "-name ", pf.fileName separator ' ') as files,
+group_concat(distinct "-name ", pe.fileName separator ' ') as exceptions
 from purges p
   left join purgeFiles pf on pf.purgeId = p.id
+  left join purgeExceptions pe on pe.purgeId = p.id
     where systemId = 1
-    and enabled = 1;
+    and enabled = 1
+      group by p.id;
 
 
 
@@ -19,3 +21,15 @@ from purges p
   left join purgeExceptions pe on pe.purgeId = p.id
     where systemId = 1
     and enabled = 1;
+
+
+select concat(p.dir,pf.fileName) as purge,
+concat(p.dir,pe.fileName) as excep
+  from purges p
+    left join purgeFiles pf on pf.purgeId = p.id
+    left join purgeExceptions pe on pe.purgeId = p.id
+      where systemId = 1
+      and enabled = 1;
+
+
+      GROUP_CONCAT(cate_id)
